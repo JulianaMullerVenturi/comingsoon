ecef371df3b71f497cb6ccb22bc31fc779e1a2fc
Juliana Muller Venturi <j.venturi@xxenta.com>
2026-04-04 18:10:01 +0200

    Countdown Working

--- script.js
+++ script.js
@@ -379,4 +379,106 @@ document.addEventListener('DOMContentLoaded', () => {
             elipse.style.transform = `translate(${mouseX}px, ${mouseY}px) translate(-50%, -50%)`;
         }
     }, 100);
+
+    // --- Countdown Timer Logic ---
+    let countdownState = {
+        days: 30,
+        hours: 6,
+        minutes: 11,
+        seconds: 39
+    };
+
+    function formatDigit(val) {
+        return val.toString().padStart(2, '0');
+    }
+
+    function updateDigit(digitId, newValue) {
+        const wrapper = document.getElementById(digitId);
+        if (!wrapper) return;
+        
+        const currentDigit = wrapper.querySelector('.digit.current');
+        
+        if (currentDigit && currentDigit.innerText === newValue) return;
+
+        if (currentDigit) {
+            currentDigit.classList.remove('current');
+            currentDigit.classList.add('fade-out');
+            
+            setTimeout(() => {
+                if (currentDigit.parentNode === wrapper) {
+                    wrapper.removeChild(currentDigit);
+                }
+            }, 800);
+        }
+
+        const newDigit = document.createElement('div');
+        newDigit.className = 'digit current fade-in';
+        if (newValue === '1') {
+            newDigit.classList.add('digit-one');
+        }
+        newDigit.innerText = newValue;
+        wrapper.appendChild(newDigit);
+
+        setTimeout(() => {
+            newDigit.classList.remove('fade-in');
+        }, 800);
+    }
+
+    function tickCountdown() {
+        if (countdownState.seconds > 0) {
+            countdownState.seconds--;
+        } else {
+            countdownState.seconds = 59;
+            if (countdownState.minutes > 0) {
+                countdownState.minutes--;
+            } else {
+                countdownState.minutes = 59;
+                if (countdownState.hours > 0) {
+                    countdownState.hours--;
+                } else {
+                    countdownState.hours = 23;
+                    if (countdownState.days > 0) {
+                        countdownState.days--;
+                    }
+                }
+            }
+        }
+
+        const daysStr = formatDigit(countdownState.days);
+        const hoursStr = formatDigit(countdownState.hours);
+        const minutesStr = formatDigit(countdownState.minutes);
+        const secondsStr = formatDigit(countdownState.seconds);
+
+        updateDigit('days-tens', daysStr[0]);
+        updateDigit('days-ones', daysStr[1]);
+        
+        updateDigit('hours-tens', hoursStr[0]);
+        updateDigit('hours-ones', hoursStr[1]);
+        
+        updateDigit('minutes-tens', minutesStr[0]);
+        updateDigit('minutes-ones', minutesStr[1]);
+        
+        updateDigit('seconds-tens', secondsStr[0]);
+        updateDigit('seconds-ones', secondsStr[1]);
+
+        syncContainerOpticalKerning('days-tens', daysStr[0], daysStr[1]);
+        syncContainerOpticalKerning('hours-tens', hoursStr[0], hoursStr[1]);
+        syncContainerOpticalKerning('minutes-tens', minutesStr[0], minutesStr[1]);
+        syncContainerOpticalKerning('seconds-tens', secondsStr[0], secondsStr[1]);
+    }
+
+    function syncContainerOpticalKerning(tensId, tensVal, onesVal) {
+        const tensWrapper = document.getElementById(tensId);
+        if (tensWrapper && tensWrapper.parentElement) {
+            const container = tensWrapper.parentElement;
+            if (tensVal === '1') container.classList.add('tens-is-one');
+            else container.classList.remove('tens-is-one');
+
+            if (onesVal === '1') container.classList.add('ones-is-one');
+            else container.classList.remove('ones-is-one');
+        }
+    }
+
+    setInterval(tickCountdown, 1000);
+
 });
